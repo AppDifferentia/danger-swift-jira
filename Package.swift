2e6f98f725eb06a278049ec0aa1524e4950c1450
Wei Sun <weisunOW@users.noreply@github.com>
2021-09-06 00:45:37 +1000

    [CI] Gather Code Coverage [Experiment]

--- Package.swift
+++ Package.swift
@@ -5,23 +5,23 @@ import PackageDescription
 let package = Package(
     name: "DangerSwiftJira",
     products: [
-        .library(
-            name: "DangerSwiftJira",
-            targets: ["DangerSwiftJira"]
-        ),
-        .library(name: "DangerDependencies", type: .dynamic, targets: ["DangerDependencies"]),
+        .library(name: "DangerSwiftJira", targets: ["DangerSwiftJira"]),
+        .library(name: "DangerDep", type: .dynamic, targets: ["DangerDependencies"]),
     ],
     dependencies: [
         .package(name: "danger-swift", url: "https://github.com/danger/swift", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/realm/SwiftLint", .upToNextMajor(from: "0.43.0")),
         .package(name: "DangerSwiftCoverage", url: "https://github.com/f-meloni/danger-swift-coverage", .upToNextMajor(from: "1.2.1")),
+        .package(url: "https://github.com/realm/SwiftLint", .upToNextMajor(from: "0.43.0")),
     ],
     targets: [
-        .target(name: "DangerDependencies", dependencies: ["danger-swift", "DangerSwiftCoverage"]),
         .target(
-            name: "DangerSwiftJira",
-            dependencies: ["danger-swift"]
+            name: "DangerDependencies",
+            dependencies: [
+                .product(name: "Danger", package: "danger-swift"),
+                "DangerSwiftCoverage"
+            ]
         ),
+        .target(name: "DangerSwiftJira", dependencies: [.product(name: "Danger", package: "danger-swift")]),
         .testTarget(
             name: "DangerSwiftJiraTests",
             dependencies: [
@@ -29,5 +29,6 @@ let package = Package(
                 .product(name: "DangerFixtures", package: "danger-swift"),
             ]
         ),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
